532afe81885b56b8b75143e426c5147f04562cc2
czh <13580518842@163.com>
2018-05-16 19:15:52 +0800

    topic模式增加json传送结构体，最好先base64编码一下

--- topics/emit_log_topic.cc
+++ topics/emit_log_topic.cc
@@ -1,4 +1,5 @@
 #include "../MyTool.h"
+#include "../json.hpp"
 #include <ev.h>
 #include <iostream>
 #include <memory.h>
@@ -9,9 +10,10 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-
+#include <strings.h>
 
 using namespace std;
+using json = nlohmann::json;
 
 const int MAXSIZE = 1024;
 
@@ -34,6 +36,14 @@ sig_int(int signo){
   exit(0);
 }
 
+typedef struct
+{
+  uint8_t a;
+  uint8_t b;
+  char c[MAXSIZE];
+
+}dataType;
+
 static void
 stdin_cb(EV_P_ ev_io* w, int revents){
   auto input = make_shared_array<char>(MAXSIZE);
@@ -49,8 +59,25 @@ stdin_cb(EV_P_ ev_io* w, int revents){
         string severity = data.substr(pos_one + 1, pos_two - pos_one - 1);
         if (find(severityList.begin(), severityList.end(), severity) != severityList.end()) {
           string routingKey = facility + "." + severity;
-          tool.GetChannel()->publish("logs_topic", routingKey, data);
-          cout << "[x] Sent " << data << " and the routingkey is " << routingKey << "\n";
+          json dataJson;
+          dataJson.emplace("facility", facility);
+          dataJson.emplace("severity", severity);
+          // 构造结构体temp
+          dataType tempStruct;
+          tempStruct.a = 12;
+          tempStruct.b = 18;
+          string realData = data.substr(pos_two + 1, data.size() - pos_two - 1);
+          bzero(tempStruct.c, MAXSIZE);
+          memcpy(tempStruct.c, realData.c_str(), realData.size());
+          // 结构体转string
+          auto tempPChar = make_shared_array<char>(sizeof(dataType));
+          memcpy(tempPChar.get(), &tempStruct, sizeof(dataType));
+          string emitData(tempPChar.get(), sizeof(dataType));
+          // 以上三句可以写为string emitData(reinterpret_cast<char*>(&tempStruct), sizeof(dataType));但不一定所以编译器都会支持
+          // dataJson.emplace("data", data.substr(pos_two + 1, data.size() - pos_two - 1));
+          dataJson.emplace("data", emitData);
+          tool.GetChannel()->publish("logs_topic", routingKey, dataJson.dump());
+          cout << "[x] Sent " << data << " and the routingkey is " << routingKey << " the json is " << dataJson <<  "\n";
         }
       }
     }
--- topics/receive_logs_topic.cc
+++ topics/receive_logs_topic.cc
@@ -1,4 +1,5 @@
 #include "../MyTool.h"
+#include "../json.hpp"
 #include <ev.h>
 #include <signal.h>             // signal()
 #include <stdlib.h>             // exit()
@@ -7,6 +8,7 @@
 #include <amqpcpp.h>
 
 using namespace std;
+using json = nlohmann::json;
 
 struct ev_loop* loop = EV_DEFAULT;
 MyTool tool(loop);
@@ -28,7 +30,7 @@ int main(int argc, char *argv[])
 
   tool.GetChannel()->declareExchange("logs_topic", AMQP::topic);
   tool.GetChannel()->declareQueue(AMQP::exclusive)
-    .onSuccess([&](const string& name, uint32_t messagecount, uint32_t consumercount){
+    .onSuccess([&](const string& name, uint32_t messagecount, uint32_t consumercount) {
         if (argc > 1) {
           for (int i = 1; i < argc; i++) {
             string bindKey = string(argv[i]);
@@ -45,6 +47,11 @@ int main(int argc, char *argv[])
           .onReceived([](const AMQP::Message &message, uint64_t deliveryTag, bool redelivered){
               string data(message.body(), message.bodySize());
               cout << "[x] Received " << data << "\n";
+              cout << "[routingkey] " << message.routingkey() << "\n";
+              auto dataJson = json::parse(data);
+              cout << "facility is " << dataJson["facility"].get<string>() << "\n";
+              cout << "severity is " << dataJson["severity"].get<string>() << "\n";
+              cout << "data is " << dataJson["data"].get<string>() << "\n";
             });
       });
 
